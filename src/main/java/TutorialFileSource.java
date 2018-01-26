3b02d0907c43ce3d218a9153049618812b6df377
Gunnnn <metallalive@gmail.com>
2018-01-26 18:13:34 +0300

    from r

--- src/main/java/TutorialFileSource.java
+++ src/main/java/TutorialFileSource.java
@@ -1,8 +1,12 @@
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.Node;
 import org.graphstream.graph.implementations.DefaultGraph;
+import org.graphstream.graph.implementations.MultiGraph;
 import org.graphstream.stream.file.FileSource;
 import org.graphstream.stream.file.FileSourceFactory;
+import org.graphstream.ui.spriteManager.Sprite;
+import org.graphstream.ui.spriteManager.SpriteManager;
+import org.graphstream.ui.view.Viewer;
 
 import java.io.IOException;
 
@@ -11,12 +15,19 @@ public class TutorialFileSource {
     private static String styleSheet;
 
     public static void main(String ... args) {
-        styleSheet = "node {" +
-                "fill-color: black;}" +
-                "node.marked {" +
-                "	fill-color: red;" +
+        styleSheet = "node {\n" +
+                "\tfill-color: green;\n" +
+                "\tsize: 10px;\n" +
+                "\tstroke-mode: plain;\n" +
+                "\tstroke-color: black;\n" +
+                "\tstroke-width: 1px;\n" +
+                "}\n" +
+                "\n" +
+                "node.important {\n" +
+                "\tfill-color: red;\n" +
+                "\tsize: 15px;\n" +
                 "}";
-        String filePath = "C:/Temp/dot_2.dot";
+        String filePath = "C:/Temp/dot_1.dot";
         Graph graph = new DefaultGraph("graph");
         FileSource fs = null;
         try {
@@ -26,7 +37,6 @@ public class TutorialFileSource {
         }
 
         fs.addSink(graph);
-
 
         try {
             fs.begin(filePath);
@@ -48,13 +58,23 @@ public class TutorialFileSource {
 
         graph.addAttribute("ui.stylesheet", styleSheet);
         graph.setAutoCreate(true);
-//        graph.setStrict(false);
+        graph.setStrict(false);
         graph.display();
 
+        SpriteManager sman = new SpriteManager(graph);
+        Sprite s = sman.addSprite("S11");
 
+        //addind labels for each node and doing all work
         for (Node node : graph) {
             node.addAttribute("ui.label", node.getId());
+//            System.out.println("'" + node.getId().toString() + "'");
+            s.attachToNode(node.getId().toString());
+            if (node.getId().toString().contains("B2MR_RISK_CONC"))
+            {
+                node.addAttribute("ui.class", "important"); // make the node appear as important.
+            }
         }
+
 
     }
 }
